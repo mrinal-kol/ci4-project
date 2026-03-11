d49f3d49415fef54ed3a8de7aa996536968e402f
dev <ronojit.dev20151@gmail.com>
2026-03-11 10:23:44 +0530

    daily_work_11-03-2026

--- app/Views/viewpage.php
+++ app/Views/viewpage.php
@@ -4,42 +4,90 @@
 <?php
     $data = $studentList;
    ?>
-   <form action="<?=base_url('hello/update')?>" method="post" enctype="multipart/form-data">
+   <form action="<?= base_url('hello/update') ?>" method="post" enctype="multipart/form-data">
+
         <input type="hidden" name="id" value="<?= isset($data['id']) ? $data['id'] : '' ?>" />
+
         <table class='table'>
+
             <tr>
-                <td>Name</td>
-                <td><input type="text" name='name' value='<?= isset($data['name']) ? $data['name'] : '' ?>'/></td>
+            <td>Name</td>
+            <td>
+            <input type="text" name="name" value="<?= old('name', $data['name'] ?? '') ?>" />
+            <?php if(session('errors.name')): ?>
+            <div style="color:red"><?= session('errors.name') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td>roll</td>
-                <td><input type="text" name='roll' value='<?= isset($data['roll']) ? $data['roll'] : '' ?>'/></td>
+            <td>Roll</td>
+            <td>
+            <input type="text" name="roll" value="<?= old('roll', $data['roll'] ?? '') ?>" />
+            <?php if(session('errors.roll')): ?>
+            <div style="color:red"><?= session('errors.roll') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td>class</td>
-                <td><input type="text" name='class' value='<?= isset($data['class']) ? $data['class'] : '' ?>'/></td>
+            <td>Class</td>
+            <td>
+            <input type="text" name="class" value="<?= old('class', $data['class'] ?? '') ?>" />
+            <?php if(session('errors.class')): ?>
+            <div style="color:red"><?= session('errors.class') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td>email</td>
-                <td><input type="text" name='email' value='<?= isset($data['email']) ? $data['email'] : '' ?>'/></td>
+            <td>Email</td>
+            <td>
+            <input type="text" name="email" value="<?= old('email', $data['email'] ?? '') ?>" />
+            <?php if(session('errors.email')): ?>
+            <div style="color:red"><?= session('errors.email') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td>phone</td>
-                <td><input type="text" name='phone' value='<?= isset($data['phone']) ? $data['phone'] : '' ?>'/></td>
+            <td>Phone</td>
+            <td>
+            <input type="text" name="phone" value="<?= old('phone', $data['phone'] ?? '') ?>" />
+            <?php if(session('errors.phone')): ?>
+            <div style="color:red"><?= session('errors.phone') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td>section</td>
-                <td><input type="text" name='section' value='<?= isset($data['section']) ? $data['section'] : '' ?>'/></td>
+            <td>Section</td>
+            <td>
+            <input type="text" name="section" value="<?= old('section', $data['section'] ?? '') ?>" />
+            <?php if(session('errors.section')): ?>
+            <div style="color:red"><?= session('errors.section') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td>Pic</td>
-                <td><input type="file" name='myfile' /></td>
+            <td>Pic</td>
+            <td>
+            <input type="file" name="myfile" />
+            <?php if(session('errors.myfile')): ?>
+            <div style="color:red"><?= session('errors.myfile') ?></div>
+            <?php endif; ?>
+            </td>
             </tr>
+
             <tr>
-                <td colspan="2" align='center'><input class='btn btn-primary' type="submit" name='update' value='update' /></td>
+            <td colspan="2" align="center">
+            <input class="btn btn-primary" type="submit" name="update" value="update" />
+            </td>
             </tr>
+
         </table>
-   </form>
+    </form>
 
    <?php if (isset($_GET['s']) && $_GET['s'] == 1): ?>
     <div class="alert alert-success">
